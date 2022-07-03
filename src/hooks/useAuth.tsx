de3cc99b957a39ee2b6a1cef4cdfcf3773d43331
F2253474 Daniel Braga Moura <danielbraga@bb.com.br>
2022-07-03 10:51:08 -0300

    ajustes login social twitch

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -4,6 +4,8 @@ import { generateRandom } from 'expo-auth-session/build/PKCE';
 
 import { api } from '../services/api';
 
+const { CLIENT_ID } = process.env;
+
 interface User {
   id: number;
   display_name: string;
@@ -36,61 +38,69 @@ function AuthProvider({ children }: AuthProviderData) {
   const [user, setUser] = useState({} as User);
   const [userToken, setUserToken] = useState('');
 
-  // get CLIENT_ID from environment variables
+ 
 
   async function signIn() {
     try {
-      // set isLoggingIn to true
+      setIsLoggingIn(true);
 
-      // REDIRECT_URI - create OAuth redirect URI using makeRedirectUri() with "useProxy" option set to true
-      // RESPONSE_TYPE - set to "token"
-      // SCOPE - create a space-separated list of the following scopes: "openid", "user:read:email" and "user:read:follows"
-      // FORCE_VERIFY - set to true
-      // STATE - generate random 30-length string using generateRandom() with "size" set to 30
+      const REDIRECT_URI = makeRedirectUri({ useProxy: true });;
+      const RESPONSE_TYPE = `token`;
+      const SCOPE = encodeURI(`openid user:read:email user:read:follows`);
+      const FORCE_VERIFY = true;
+      const STATE = generateRandom(30);
 
-      // assemble authUrl with twitchEndpoint authorization, client_id, 
-      // redirect_uri, response_type, scope, force_verify and state
+      
+      const twitchEndpoint = `${twitchEndpoints.authorization}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}&force_verify=${FORCE_VERIFY}&state=${STATE}`;
+      const response = await startAsync(twitchEndpoint);
 
-      // call startAsync with authUrl
+      if (response.type === 'success'&& response.params.error !== 'access_denied'){
+        if (response.params.state !== STATE){
+          throw new Error('Invalid state value');
+        }
 
-      // verify if startAsync response.type equals "success" and response.params.error differs from "access_denied"
-      // if true, do the following:
+        api.defaults.headers.common['Authorization'] = `Bearer ${response.params.access_token}`;
 
-        // verify if startAsync response.params.state differs from STATE
-        // if true, do the following:
-          // throw an error with message "Invalid state value"
+        const userResponse = await api.get('/users');
 
-        // add access_token to request's authorization header
+        const {id, display_name, email, profile_image_url} = userResponse.data.data[0];
 
-        // call Twitch API's users route
+        setUser({
+          id,
+          display_name,
+          email,
+          profile_image_url
+        });
 
-        // set user state with response from Twitch API's route "/users"
-        // set userToken state with response's access_token from startAsync
-    } catch (error) {
-      // throw an error
+        setUserToken(response.params.access_token);
+
+      }
+    } catch (error: any) {
+      throw new Error(error);
     } finally {
-      // set isLoggingIn to false
+      setIsLoggingIn(false);
     }
   }
 
   async function signOut() {
     try {
-      // set isLoggingOut to true
-
-      // call revokeAsync with access_token, client_id and twitchEndpoint revocation
+       setIsLoggingOut(true);
+       revokeAsync({
+        token: userToken,
+        clientId: CLIENT_ID,
+        revocationEndpoint: twitchEndpoints.revocation
+       });
     } catch (error) {
     } finally {
-      // set user state to an empty User object
-      // set userToken state to an empty string
-
-      // remove "access_token" from request's authorization header
-
-      // set isLoggingOut to false
+      setUser({});
+      setUserToken('');
+      delete api.defaults.headers.common['Authorization'];
+      setIsLoggingOut(false);
     }
   }
 
   useEffect(() => {
-    // add client_id to request's "Client-Id" header
+    api.defaults.headers.common['Client-Id'] = CLIENT_ID;
   }, [])
 
   return (
